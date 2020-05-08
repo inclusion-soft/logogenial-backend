7fc8f6467882984834b02e6ce5edd9dc9b6f5ca2
Carlos Romero <soportecarlosromero@gmail.com>
2020-05-07 23:24:31 -0500

    inicial

---
+++ src/main/java/com/rc/logenialbackend/LogogenialBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rc.logenialbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogogenialBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogogenialBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rc/logenialbackend/LogogenialBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rc.logenialbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogogenialBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
