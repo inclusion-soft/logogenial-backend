affea09e147960af61c26731c80e77ed706285c8
Carlos Romero <soportecarlosromero@gmail.com>
2020-06-20 09:26:42 -0500

    Add swagger al proyecto

--- src/main/java/com/rc/logenialbackend/auth/SpringSecurityConfig.java
+++ src/main/java/com/rc/logenialbackend/auth/SpringSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.rc.logenialbackend.auth;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,10 +13,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableGlobalMethodSecurity(securedEnabled=true)
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	Logger log = LoggerFactory.getLogger(SpringSecurityConfig.class);
 
 	@Autowired
 	private UserDetailsService usuarioService;
@@ -43,6 +57,33 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 		.and()
 		.csrf().disable()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+
+	/**
+	 * Main config.
+	 *
+	 * @return the docket
+	 */
+	@Bean
+	public Docket mainConfig() {
+
+		log.debug("Starting Swagger");
+		springfox.documentation.service.Contact contact = new Contact("Logogenial-app", "http://www.carlosromero.net",
+				"ncamacho44@uan.edu.co ");
+
+		@SuppressWarnings("rawtypes")
+		List<VendorExtension> vext = new ArrayList<VendorExtension>();
+
+		ApiInfo apiInfo = new ApiInfo("Logogenial - Microservicio gestor-preguntas", "ITO GENERATOR", "1.0", "http://www.ito-software.com", contact,
+				"ITO SOFTWARE SAS", "http://www.carlosromero.net", vext);
+
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+
 	}
 
 }
