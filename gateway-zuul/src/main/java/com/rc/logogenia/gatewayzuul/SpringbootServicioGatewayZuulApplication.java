4f711b56c98d7d4c0f9641ff4231cd9803a082da
Carlos Romero <soportecarlosromero@gmail.com>
2020-08-05 22:44:07 -0500

    agrega proxy zuul

---
+++ gateway-zuul/src/main/java/com/rc/logogenia/gatewayzuul/SpringbootServicioGatewayZuulApplication.java
@@ -0,0 +1,17 @@
+package com.rc.logogenia.gatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class SpringbootServicioGatewayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootServicioGatewayZuulApplication.class, args);
+    }
+
+}
---
+++ gateway-zuul/src/test/java/com/rc/logogenia/gatewayzuul/SpringbootServicioGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rc.logogenia.gatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioGatewayZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
